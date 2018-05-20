a672ed93b7effa58786aa90b4670a7ac03ba5d38
rpedsel <rpedsel@gmail.com>
2018-05-19 17:56:42 -0700

    change location of browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Posts from './containers/Posts';
 import Shortener from './containers/Shortener';
 import Message from './containers/Message';
+import Layout from './hoc/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/posts" component={Posts} />
-            <Route path="/" exact component={Shortener} />
-            <Route path="/:id" component={Message} />
-          </Switch>
-        </BrowserRouter>
+        <Layout>
+            <Switch>
+              <Route path="/posts" component={Posts} />
+              <Route path="/" exact component={Shortener} />
+              <Route path="/:id" component={Message} />
+            </Switch>
+        </Layout>
       </div>
     );
   }
