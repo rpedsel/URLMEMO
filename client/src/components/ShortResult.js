e1219535b66fad30b723965a5122548d1e04a5a5
rpedsel <rpedsel@gmail.com>
2018-05-28 14:14:34 -0700

    shortResult component

---
+++ client/src/components/ShortResult.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../style.css';
+
+const shortResult = (props) => (
+    <div className='grid'>
+    <div className='centered grid__col--8'>
+    <p>Your shortened URL is: <strong>{props.short_url}</strong></p>
+    </div>
+    </div>
+);
+
+export default shortResult;
